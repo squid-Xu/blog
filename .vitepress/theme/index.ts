2a57b60229d3b3544abc5cfcda727f4d937a126f
徐梦强 <113534980@qq.com>
2024-07-20 16:53:26 +0800

    增加评论

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,13 +1,20 @@
+import { h } from 'vue';
 import DefaultTheme from 'vitepress/theme';
 import { onMounted, watch, nextTick } from 'vue';
 import { useRoute } from 'vitepress';
 import mediumZoom from 'medium-zoom';
 import CustomLayout from './layouts/CustomLayout.vue';
+import comment from './components/comment.vue';
 
 import './index.css';
 
 export default {
 	...DefaultTheme,
+	Layout: () => {
+		return h(DefaultTheme.Layout, null, {
+			'doc-after': () => h(comment),
+		});
+	},
 	enhanceApp({ app }) {
 		app.component('CustomLayout', CustomLayout);
 	},
