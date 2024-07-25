6aa5768e556d33a66417e3a28f067586aec32503
徐梦强 <1135349080@qq.com>
2024-07-25 08:21:47 +0800

    11

--- nav/tool/data.js
+++ nav/tool/data.js
@@ -5,4 +5,10 @@ export const OPEN_DATA = [
 		desc: '随机身份证号码生成',
 		link: 'https://sfz.fatcarter.cn/index.shtml',
 	},
+	{
+		icon: 'https://uutool.cn/assets/images/favicon.png',
+		title: 'UU在线工具',
+		desc: '便捷实用的工具集合站',
+		link: 'https://uutool.cn',
+	},
 ];
